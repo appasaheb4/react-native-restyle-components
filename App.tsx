1f3daaaf6bb3a533b15a02034090dd8367ae1712
appasaheb4 <onlyappasaheb4@gmail.com>
2022-06-25 10:31:13 +0530

    PanResponder added.

--- App.tsx
+++ App.tsx
@@ -1,44 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import {SafeAreaView, StatusBar, View} from 'react-native';
+import React, {useRef} from 'react';
+import {View, PanResponder} from 'react-native';
 // here you can use  'react-native-restyle-components' not './src/library/components'
 import {OtpVerify, Dropdown} from './src/library/components';
 
 const App = () => {
+  const productTypeRef = useRef<any>();
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponderCapture: (evt: any) => {
+        const inst = evt._targetInst;
+        console.log({inst});
+
+        if (Array.isArray(inst?.memoizedProps?.children)) {
+          productTypeRef.current?.hidePoup();
+        }
+        return false;
+      },
+    }),
+  ).current;
+
   return (
-    <SafeAreaView>
-      <StatusBar barStyle={'light-content'} />
-      <View style={{flex: 1}}>
-        <OtpVerify
-          onVerify={otp => {
-            console.log({otp});
-          }}
-        />
-        <View style={{marginTop: 40}} />
-        <Dropdown
-          label={'Product Type'}
-          onSelect={item => {
-            console.log({item});
-          }}
-          data={[
-            {label: 'tech abl', value: 'tech abl'},
-            {label: 'github.com/appasaheb4', value: 'github.com/appasaheb4'},
-          ]}
-          displayKey="label"
-          testID="PROD-TYPE"
-          zIndex={1}
-        />
-      </View>
-    </SafeAreaView>
+    <View style={{flex: 1, marginTop: 44}} {...panResponder.panHandlers}>
+      <OtpVerify
+        onVerify={otp => {
+          console.log({otp});
+        }}
+      />
+      <View style={{marginTop: 40}} />
+      <Dropdown
+        // ref={productTypeRef}
+        label={'Product Type'}
+        onSelect={item => {
+          console.log({item});
+        }}
+        data={[
+          {label: 'tech abl', value: 'tech abl'},
+          {label: 'github.com/appasaheb4', value: 'github.com/appasaheb4'},
+        ]}
+        displayKey="label"
+        testID="PROD-TYPE"
+        zIndex={1}
+      />
+    </View>
   );
 };
 

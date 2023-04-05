a3e99a0685a3496011e2cf0ce202eee6b1431e0a
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2023-04-05 18:11:43 +0530

    step comp modify

--- src/library/components/molecules/stepper/stepper.component.tsx
+++ src/library/components/molecules/stepper/stepper.component.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
-import {Box, TextRN} from '../..';
+import {Text} from 'react-native';
+import {Box} from '../..';
 import LinearGradient from 'react-native-linear-gradient';
 
 interface StepperGradientProps {
@@ -39,41 +40,7 @@ export const StepperGradient = ({
         end={{x: 0.5, y: 1.0}}
         locations={[0, 0.5, 0.6]}
         colors={colors}>
-        {content.map((item, index) => (
-          <>
-            <Box key={index}>
-              <Box
-                backgroundColor={index + 1 === active ? 'white' : 'transparent'}
-                borderColor="white"
-                borderWidth={1}
-                height={stepperSize}
-                width={stepperSize}
-                borderRadius={stepperSize / 2}
-              />
-              <TextRN
-                color="white"
-                fontSize={8}
-                style={{
-                  position: 'absolute',
-                  width: 100,
-                  color: 'white',
-                  marginLeft: -20,
-                  marginTop: 12,
-                }}>
-                {item}
-              </TextRN>
-            </Box>
-
-            {content.length !== index + 1 && (
-              <Box
-                flex={1}
-                backgroundColor="white"
-                height={1}
-                borderColor="white"
-              />
-            )}
-          </>
-        ))}
+        <Text>HI hello</Text>
       </LinearGradient>
     </Box>
   );

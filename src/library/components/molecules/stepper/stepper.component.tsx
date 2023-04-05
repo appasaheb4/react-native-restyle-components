820f1b0a0eb761b400612bfe757f708a7cf3c5ef
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2023-04-05 18:29:30 +0530

    lib update

--- src/library/components/molecules/stepper/stepper.component.tsx
+++ src/library/components/molecules/stepper/stepper.component.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable import/prefer-default-export */
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Box, TextRN} from '../..';
+import LinearGradient from 'react-native-linear-gradient';
 
 interface StepperGradientProps {
   active: number;
@@ -17,8 +20,61 @@ export const StepperGradient = ({
   stepperSize = 10,
 }: StepperGradientProps) => {
   return (
-    <View>
-      <Text>HI hello</Text>
-    </View>
+    <Box
+      flexDirection="row"
+      alignItems="center"
+      justifyContent="space-between"
+      marginVertical="md">
+      <LinearGradient
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          height: height,
+          borderRadius: 5,
+          flexDirection: 'row',
+          paddingHorizontal: 30,
+        }}
+        start={{x: 0.0, y: 0.25}}
+        end={{x: 0.5, y: 1.0}}
+        locations={[0, 0.5, 0.6]}
+        colors={colors}>
+        {content.map((item, index) => (
+          <>
+            <Box key={index}>
+              <Box
+                backgroundColor={index + 1 === active ? 'white' : 'transparent'}
+                borderColor="white"
+                borderWidth={1}
+                height={stepperSize}
+                width={stepperSize}
+                borderRadius={stepperSize / 2}
+              />
+              <TextRN
+                color="white"
+                fontSize={8}
+                style={{
+                  position: 'absolute',
+                  width: 100,
+                  color: 'white',
+                  marginLeft: -20,
+                  marginTop: 12,
+                }}>
+                {item}
+              </TextRN>
+            </Box>
+
+            {content.length !== index + 1 && (
+              <Box
+                flex={1}
+                backgroundColor="white"
+                height={1}
+                borderColor="white"
+              />
+            )}
+          </>
+        ))}
+      </LinearGradient>
+    </Box>
   );
 };

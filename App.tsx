ab60046fa9a68ae09c9a8746e61ef141eeee9070
Lakade Appasaheb <onlyappasaheb4@gmail.com>
2023-04-05 18:50:33 +0530

    remove root

--- App.tsx
+++ App.tsx
@@ -16,8 +16,8 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import {ThemeProvider} from '@shopify/restyle';
-import {theme} from './src/theme';
+// import {ThemeProvider} from '@shopify/restyle';
+// import {theme} from './src/theme';
 
 //import {OtpVerify, Dropdown, StepperGradient} from './src/library/components';
 
@@ -34,32 +34,32 @@ function App(): JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={{padding: 10}}>
-        <ThemeProvider theme={theme}>
-          <OtpVerify
-            onVerify={otp => {
-              console.log({otp});
-            }}
-          />
-          <Dropdown
-            label={'Product Type'}
-            onSelect={item => {
-              console.log({item});
-            }}
-            data={[
-              {label: 'tech abl', value: 'tech abl'},
-              {label: 'github.com/appasaheb4', value: 'github.com/appasaheb4'},
-            ]}
-            displayKey="label"
-            testID="PROD-TYPE"
-            zIndex={1}
-          />
-          <StepperGradient
-            active={1}
-            colors={['#0687B4', '#D92CC1']}
-            stepperSize={14}
-            content={['Your Cart', 'About You', 'Payment']}
-          />
-        </ThemeProvider>
+        {/* <ThemeProvider theme={theme}> */}
+        <OtpVerify
+          onVerify={otp => {
+            console.log({otp});
+          }}
+        />
+        <Dropdown
+          label={'Product Type'}
+          onSelect={item => {
+            console.log({item});
+          }}
+          data={[
+            {label: 'tech abl', value: 'tech abl'},
+            {label: 'github.com/appasaheb4', value: 'github.com/appasaheb4'},
+          ]}
+          displayKey="label"
+          testID="PROD-TYPE"
+          zIndex={1}
+        />
+        <StepperGradient
+          active={1}
+          colors={['#0687B4', '#D92CC1']}
+          stepperSize={14}
+          content={['Your Cart', 'About You', 'Payment']}
+        />
+        {/* </ThemeProvider> */}
       </ScrollView>
     </SafeAreaView>
   );

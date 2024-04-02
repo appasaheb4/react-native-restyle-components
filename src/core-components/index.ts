52abd244c105230618b8df1d36ad06bc987c8c87
Lakade Appasaheb Balu <appasaheb4@Lakades-MacBook-Pro.local>
2024-04-02 13:38:27 +0530

    default lib install

--- src/core-components/index.ts
+++ src/core-components/index.ts
@@ -4,4 +4,4 @@ export * from './atoms/text.component';
 
 export * from './molecules/otp-verify/otp-verify.component';
 export * from './molecules/stepper/stepper.component';
-// export * from './molecules/drop-down/drop-down.component';
+export * from './molecules/drop-down/drop-down.component';

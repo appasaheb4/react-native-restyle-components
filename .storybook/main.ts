5e33429a479223275854f891ba524a7bdde7e6ec
Lakade Appasaheb Balu <appasaheb4@Lakades-MacBook-Pro.local>
2024-04-23 14:59:22 +0530

    stories drop down list added.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,14 @@
-import { StorybookConfig } from '@storybook/react-native';
+import {StorybookConfig} from '@storybook/react-native';
 
 const main: StorybookConfig = {
-  stories: ['./stories/**/*.stories.?(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-ondevice-controls', '@storybook/addon-ondevice-actions'],
+  stories: [
+    './stories/**/*.stories.?(ts|tsx|js|jsx)',
+    '../src/core-components/**/*.stories.?(ts|tsx|js|jsx)',
+  ],
+  addons: [
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-actions',
+  ],
 };
 
 export default main;
---
+++ src/core-components/molecules/drop-down-check-box-parent-child/drop-down-check-box-parent-child.stories.tsx
@@ -0,0 +1,60 @@
+import {View} from 'react-native';
+import type {Meta, StoryObj} from '@storybook/react';
+import {DropDownCheckBoxParentChild} from './drop-down.check-box-parent-child.component';
+
+const fn = (item?: any) => {
+  console.log({item});
+};
+
+const DropDownCheckBoxParentChildMeta: Meta<
+  typeof DropDownCheckBoxParentChild
+> = {
+  title: 'Design System/Molecules/DropDownCheckBoxParentChild',
+  component: DropDownCheckBoxParentChild,
+  decorators: [
+    Story => (
+      <View style={{flex: 1, padding: 10}}>
+        <Story />
+      </View>
+    ),
+  ],
+};
+
+export default DropDownCheckBoxParentChildMeta;
+export const Basic: StoryObj<typeof DropDownCheckBoxParentChildMeta> = {
+  args: {
+    title: 'Business Unit *',
+    data: [
+      [
+        {
+          'Parent Category 1': [
+            {title: 'Sub-Category 1', selected: false},
+            {title: 'Sub-Category 2', selected: false},
+          ],
+          selected: false,
+        },
+      ],
+      [
+        {
+          'Parent Category 2': [
+            {title: 'Sub-Category 1', selected: false},
+            {title: 'Sub-Category 2', selected: false},
+          ],
+          selected: false,
+        },
+      ],
+      [
+        {
+          'Parent Category 3': [
+            {title: 'Sub-Category 1', selected: false},
+            {title: 'Sub-Category 2', selected: false},
+          ],
+          selected: false,
+        },
+      ],
+    ],
+    onChange: item => {
+      console.log({item});
+    },
+  },
+};
